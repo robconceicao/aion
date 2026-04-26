8b9cb4776aae8148b49ee7a0253be3ca576a2acd
Roberto Tadeu <robtconceicao@gmail.com>
2026-04-26 04:19:46 -0300

    feat: corrigido erro de sintaxe do Flutterr.

--- frontend/lib/src/features/onboarding/presentation/onboarding_screen.dart
+++ frontend/lib/src/features/onboarding/presentation/onboarding_screen.dart
@@ -169,6 +169,8 @@ class _OnboardingScreenState extends State<OnboardingScreen> with SingleTickerPr
                       ],
                     ),
                   ),
+                    ),
+                  ),
                 ),
               ),
             );
